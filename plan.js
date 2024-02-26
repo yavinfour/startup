3729eb66d792a011de5cc11b8a241ce804984002
yavinfour <malincayman@gmail.com>
2024-02-26 10:53:06 -0700

    Automated state notifications

--- plan.js
+++ plan.js
@@ -10,6 +10,21 @@
     "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", 
     "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
   ];
+
+  let stateFact = [
+    "Alabama is considered part of the south", "Alaska is super cold and there are bears", "Arizona is the only place where saguaro cacti grow naturally", 
+    "Arkansas is pronounced different than Kansas", "California is a costal state famous for its oranges", 
+    "Colorado is a popular ski location", "Connecticut is super tiny", "Deleware has a lot of US history", "Florida contains the first settled city in America", 
+    "Georgia is famous for its peaches", "Hawaii is a popular tourist destination with lots of surfing", "Idaho grows a lot of potatoes", 
+    "Illinois was home to the saints for a few years before they were kicked out", "Indiana has the famous indie 500 race", "Iowa is a large farm state", 
+    "Kansas grows a lot of wheat", "Kentucky fried chicken", "Louisiana home of the mardi gras carnival", "Maine has the best lobster sandwiches", 
+    "Maryland is where the capital of the US is found", "Massachusets has Boston which is pretty cool", "Michigan", "Minnesota", "Missouri", "Mississippi", 
+    "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", 
+    "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", 
+    "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", 
+    "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", 
+    "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+  ];
   
   class Game {
     buttons;
@@ -59,6 +74,17 @@
     const stateImage = document.createElement('img');
     stateImage.classList.add('state');
     stateImage.src = 'US States/${state}.png';
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const cardTitle = document.createElement('h4');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = '${state}';
+
+    const cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    cardText.textContent = '${stateFact}';
 
     newFacts.innerHTML =
     '<div class="card" style="width:300px">' +
