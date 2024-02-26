bbd046c7c781528662a07a8cc59b28903e07c5b6
yavinfour <malincayman@gmail.com>
2024-02-25 21:08:58 -0700

    scroll bar

--- plan.js
+++ plan.js
@@ -13,7 +13,7 @@
         }
     
         getPlayerName() {
-            return localStorage.getItem('userName') ?? 'Mystery player';
+            return localStorage.getItem('userName') ?? 'New User';
         }
   }
   
@@ -25,7 +25,7 @@
     const score = Math.floor(Math.random() * 3000);
     const chatText = document.querySelector('#player-messages');
     chatText.innerHTML =
-      `<div class="event"><span class="player-event">Eich</span> scored ${score}</div>` +
+      `<div class="note"><span class="player-event">Eich</span> scored ${score}</div>` +
       chatText.innerHTML;
   }, 5000);
   
