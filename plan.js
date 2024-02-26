804d658c7e09f13f60422a3c801c7f5a3ae229d0
yavinfour <malincayman@gmail.com>
2024-02-25 22:28:19 -0700

    Automated state notifications

--- plan.js
+++ plan.js
@@ -1,6 +1,15 @@
   let states = [
-    ""
-  ]
+    "Alabama", "Alaska", "Arizona", "Arkansas", "California", 
+    "Colorado", "Connecticut", "Deleware", "Florida", "Georgia", 
+    "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", 
+    "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", 
+    "Massachusets", "Michigan", "Minnesota", "Missouri", "Mississippi", 
+    "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", 
+    "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", 
+    "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", 
+    "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", 
+    "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+  ];
   
   class Game {
     buttons;
@@ -26,9 +35,9 @@
   // Simulate chat messages that will come over WebSocket
   setInterval(() => {
     const score = Math.floor(Math.random() * 49);
-    const chatText = document.querySelector('#player-messages');
+    const chatText = document.querySelector('#user-messages');
     chatText.innerHTML =
-      `<div class="note"><span class="player-event">Eich</span> scored ${score}</div>` +
+      `<div class="note"><span class="user-event">Amy</span> added ${states[score]}</div>` +
       chatText.innerHTML;
   }, 5000);
   
