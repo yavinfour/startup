d7b55f13fc1afe30f57b533a3c43dd44c3235f36
yavinfour <malincayman@gmail.com>
2024-02-25 19:56:42 -0700

    Bring over name

--- login.js
+++ login.js
@@ -1,5 +1,9 @@
 function login() {
-  const nameEl = document.querySelector("#name");
-  localStorage.setItem("userName", nameEl.value);
+  const nameElem = document.querySelector("#name");
+  const passwordElem = document.querySelector("#password");
+
+  localStorage.setItem("userName", nameElem.value);
+  localStorage.setItem("userPassword", passwordElem.value);
+
   window.location.href = "play.html";
 }
---
+++ plan.js
@@ -0,0 +1,31 @@
+  
+  class Game {
+    buttons;
+    allowPlayer;
+    sequence;
+    playerPlaybackPos;
+    mistakeSound;
+  
+    constructor() {
+
+        const playerNameEl = document.querySelector('.player-name');
+            playerNameEl.textContent = this.getPlayerName();
+        }
+    
+        getPlayerName() {
+            return localStorage.getItem('userName') ?? 'Mystery player';
+        }
+  }
+  
+  const game = new Game();
+
+  
+  // Simulate chat messages that will come over WebSocket
+  setInterval(() => {
+    const score = Math.floor(Math.random() * 3000);
+    const chatText = document.querySelector('#player-messages');
+    chatText.innerHTML =
+      `<div class="event"><span class="player-event">Eich</span> scored ${score}</div>` +
+      chatText.innerHTML;
+  }, 5000);
+  
